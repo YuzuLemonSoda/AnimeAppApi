57d88e7aa7d065e6ab138be8ab1615677a5948fd
Amado Azua III <amado.azua3@gmail.com>
2023-03-06 20:57:43 -0600

    change query to only 5 items from anime collection

--- src/main/java/com/anime/AnimeAppApi/controller/AnimeController.java
+++ src/main/java/com/anime/AnimeAppApi/controller/AnimeController.java
@@ -58,13 +58,13 @@ public class AnimeController {
         }
     }
 
-    @GetMapping("/anime/rankingTop10")
+    @GetMapping("/anime/rankingTop5")
     public ResponseEntity<List<Anime>> getAnimeByRanking() {
 
             Query query = new Query();
-            query.addCriteria(Criteria.where("ranking").lt(11));
-            List<Anime> animeTop10 = mongoTemplate.find(query, Anime.class);
-            return new ResponseEntity<>(animeTop10, HttpStatus.OK);
+            query.addCriteria(Criteria.where("ranking").lt(6));
+            List<Anime> animeTop5 = mongoTemplate.find(query, Anime.class);
+            return new ResponseEntity<>(animeTop5, HttpStatus.OK);
     }
 
 }
